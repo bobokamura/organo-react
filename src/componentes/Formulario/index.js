7a056bf7a6d8cfb7723ad7b20ac6821c753ea877
bobokamura <roberto.okamura@cotefacil.com>
2022-12-02 14:58:58 -0300

    Create project

---
+++ src/componentes/Formulario/index.js
@@ -0,0 +1,20 @@
+import CampoTexto from "../CampoTexto";
+import "./index.css";
+
+const Formulario = () => {
+  return (
+    <section className="formulario">
+      <form>
+        <h2>Preencha os dados para criar o card do colaborador.</h2>
+        <CampoTexto label="Nome" placeholder="Digite seu nome..." />
+        <CampoTexto label="Cargo" placeholder="Digite seu cargo..." />
+        <CampoTexto
+          label="Imagem"
+          placeholder="Informe o endereço da imagem..."
+        />
+        <select></select>
+      </form>
+    </section>
+  );
+};
+export default Formulario;
